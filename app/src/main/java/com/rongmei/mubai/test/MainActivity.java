3e197a8158e8f148b36283dfca5bec29db8746e7
wuyinlei <power1118wu@gmail.com>
2017-07-21 10:39:05 +0800

    简单的测试

--- app/src/main/java/com/rongmei/mubai/test/MainActivity.java
+++ app/src/main/java/com/rongmei/mubai/test/MainActivity.java
@@ -2,6 +2,8 @@ package com.rongmei.mubai.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.bt_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "点击我了", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
